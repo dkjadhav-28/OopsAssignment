a05dfb885aad934ec7a9802720be34d4f21a320b
DHANANJAY KISAN JADHAV <79322998+dkjadhav-28@users.noreply.github.com>
2024-02-17 12:01:42 +0530

    Update BankApplicationUI.java
    identify and resolve errors

--- BankApplicationUI.java
+++ BankApplicationUI.java
@@ -4,10 +4,18 @@ import java.awt.*;
 
 //this is main entry point or UI for bank data
 public class BankApplicationUI {
-    RegularUser customer_1 = new RegularUser("Dhananjay",0,0,"regular");
 
-    customer_1.deposit(1000);
-    customer_1.withdraw(100);
+
+    public static void main(String[] args) {
+        RegularUser customer_1 = new RegularUser("Dhananjay",0,0,"regular");
+        customer_1.deposit(1000);
+        customer_1.withdraw(100);
+
+        customer_1.display();
+    }
+
+
+
 
 
 
